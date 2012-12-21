flowplayer/flowplayer

commit b63b67e21c95d552137f0113196e502f6f2d9a27

No duplicate sounds with firefox + flash + autoplay, fixes #87

lib/engine/flash.js
@@ -26,10 +26,13 @@ flowplayer.engine.flash = function(player, root) {
 
       load: function(video) {
 
-         $("video", root).remove();
-
-         var url = video.src.replace(/&amp;/g, '%26').replace(/&/g, '%26').replace(/=/g, '%3D'),
+         var html5Tag = $("video", root),
+            url = video.src.replace(/&amp;/g, '%26').replace(/&/g, '%26').replace(/=/g, '%3D'),
             is_absolute = /^https?:/.test(url);
+
+         // html5 tag not needed (pause needed for firefox)
+         html5Tag[0].pause();
+         html5Tag.remove();
 
          // convert to absolute
          if (!is_absolute) url = $("<a/>").attr("href", url)[0].href;

test/minimal.html
@@ -28,12 +28,12 @@
 
    <!-- the player -->
    <div class="flowplayer"
-      -data-engine="flash"
+      data-engine="flash"
       data-swf="../dist/flowplayer.swf"
       data-rtmp="rtmp://s3b78u0kbtx79q.cloudfront.net/cfx/st"
       data-ratio=".417">
 
-      <video preload="none">
+      <video autoplay -preload="none">
          <source type="video/mp4"   src="http://stream.flowplayer.org/bauhaus/624x260.mp4"/>
          <source type="video/webm"  src="http://stream.flowplayer.org/bauhaus/624x260.webm"/>
          <source type="video/ogg"   src="http://stream.flowplayer.org/bauhaus/624x260.ogv"/>

commit e723b74cf7f677cca701ea2ac48bbff6d85ac7f4

Issue #183: do not call html5 video method when video is not supported
Fixes IE < 9 breakage without is-splash caused by
b63b67e21c95d552137f0113196e502f6f2d9a27 ff.

lib/engine/flash.js
@@ -31,7 +31,7 @@ flowplayer.engine.flash = function(player, root) {
             is_absolute = /^https?:/.test(url);
 
          // html5 tag not needed (pause needed for firefox)
-         html5Tag.length > 0 && html5Tag[0].pause();
+         html5Tag.length > 0 && flowplayer.support.video && html5Tag[0].pause();
          html5Tag.remove();
 
          // convert to absolute