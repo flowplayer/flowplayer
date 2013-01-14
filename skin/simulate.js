6ba5e00c44f31a9a58d2a132b84c67f359a6f1d6
Tero Piirainen <tipiirai@gmail.com>
2013-01-14 15:34:51 +0200

    Timeline width works with no-time, no-volume & other CSS fixes

--- skin/simulate.js
+++ skin/simulate.js
@@ -6,8 +6,10 @@ $(function() {
    // mouseover
    $(".flowplayer").each(function() {
 
-      var root = $(this).addClass("is-mouseout").bind("mouseenter mouseleave", function(e) {
-         if (!root.is(".no-hover")) {
+      var root = $(this).bind("mouseenter mouseleave", function(e) {
+         if (root.is(".no-hover")) {
+            root.addClass("is-mouseover").removeClass("is-mouseout");
+         } else {
             var over = e.type == "mouseenter";
             root.toggleClass("is-mouseover", over).toggleClass("is-mouseout", !over);
          }
